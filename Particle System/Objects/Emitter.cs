820dd38ff1ed6fe6cd8f4f404bf89fa89048aed1
Rittey Иттер <ritey@list.ru>
2022-12-18 18:17:14 +0800

    Реализация обновление партиклов и реализация цвета

--- Particle System/Objects/Emitter.cs
+++ Particle System/Objects/Emitter.cs
@@ -6,5 +6,45 @@ namespace Particle_System.Objects
 {
     public class Emitter
     {
+        public List<Particle> particles = new();
+
+        public int X; // координата X центра эмиттера
+        public int Y; // координата Y центра эмиттера
+        public int Direction = 0; // вектор направления в градусах куда сыпет эмиттер
+        public int Spreading = 360; // разброс частиц относительно Direction
+        public int SpeedMin = 1; // начальная минимальная скорость движения частицы
+        public int SpeedMax = 10; // начальная максимальная скорость движения частицы
+        public int RadiusMin = 2; // минимальный радиус частицы
+        public int RadiusMax = 10; // максимальный радиус частицы
+        public int LifeMin = 20; // минимальное время жизни частицы
+        public int LifeMax = 100; // максимальное время жизни частицы
+
+        public int ParticlesPerTick = 1; // количество частиц в тик
+
+        public Color ColorFrom = Color.White; // начальный цвет частицы
+        public Color ColorTo = Color.FromArgb(0, Color.Black); // конечный цвет частиц
+
+        public float GravitationX = 0;
+        public float GravitationY = 1; // пусть гравитация будет силой один пиксель за такт, нам хватит
+
+
+        public virtual void ResetParticle(Particle particle)
+        {
+            particle.Life = Particle.rand.Next(LifeMin, LifeMax);
+
+            particle.X = X;
+            particle.Y = Y;
+
+            float direction = Direction
+                + Particle.rand.Next(Spreading)
+                - Spreading / 2;
+
+            var speed = Particle.rand.Next(SpeedMin, SpeedMax);
+
+            particle.SpeedX = MathF.Cos(direction / 180 * MathF.PI) * speed;
+            particle.SpeedY = -MathF.Sin(direction / 180 * MathF.PI) * speed;
+
+            particle.Radius = Particle.rand.Next(RadiusMin, RadiusMax);
+        }
     }
 }
--- Particle System/Objects/Particle.cs
+++ Particle System/Objects/Particle.cs
@@ -27,5 +27,21 @@ namespace Particle_System.Objects
             Radius = rand.Next(2, 12);
             Life = rand.Next(20, 120);
         }
+        public virtual void Draw(Graphics g)
+        {
+            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+            float k = Math.Min(1f, Life / 100);
+            // рассчитываем значение альфа канала в шкале от 0 до 255
+            // по аналогии с RGB, он используется для задания прозрачности
+            int alpha = (int)(k * 255);
+
+            // создаем цвет из уже существующего, но привязываем к нему еще и значение альфа канала
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
+
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
     }
 }
