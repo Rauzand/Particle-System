f17304fe1a2571e32ff0fea8a82bbd35eb77b3c8
Rittey Иттер <ritey@list.ru>
2022-12-18 19:34:31 +0800

    Реализация цвета для портала

--- Particle System/Objects/Portal.cs
+++ Particle System/Objects/Portal.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Particle_System.Objects
 {
-    class Portal
+    public class Portal
     {
+        //две точки - вход и выход
+        public Point input;
+        public Point output;
+        public byte radius = 40;
+        //направление нчастиц на выходе из портала
+        public ushort direction;
+
+        //действие на пересечение частицы с входом портала
+        public Action<Particle>? OnPortalParticle;
+
+        public void Draw(Graphics g)
+        {
+            g.DrawEllipse(new Pen(Color.Red),
+                input.X - radius, input.Y - radius,
+                radius * 2, radius * 2);
+            g.DrawEllipse(new Pen(Color.Blue),
+                output.X - radius, output.Y - radius,
+                radius * 2, radius * 2);
+            g.DrawLine(new Pen(Color.YellowGreen, 2),
+                input.X, input.Y,
+                output.X, output.Y);
+        }
     }
 }
