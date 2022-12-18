f6b7ac6d0cddf6ef575dcd07f024fe3a75210a8b
Rittey Иттер <ritey@list.ru>
2022-12-18 16:34:59 +0800

    Добавление расчёта вектора скорости, добавление дополнительных классов для реализации

---
+++ Particle System/Objects/Emitter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Particle_System.Objects
+{
+    public class Emitter
+    {
+    }
+}
---
+++ Particle System/Objects/Particle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace Particle_System.Objects
+{
+    public class Particle
+    {
+        public int Radius; // радиус частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+
+        public float SpeedX; // скорость перемещения по оси X
+        public float SpeedY; // скорость перемещения по оси Y
+        public float Life; // запас здоровья частицы
+
+        public static Random rand = new();
+
+        public Particle()
+        {
+            var direction = (float)rand.Next(360);
+            var speed = 1 + rand.Next(10);
+
+            // рассчитываем вектор скорости
+            SpeedX = MathF.Cos(direction / 180 * MathF.PI) * speed;
+            SpeedY = -MathF.Sin(direction / 180 * MathF.PI) * speed;
+
+            Radius = rand.Next(2, 12);
+            Life = rand.Next(20, 120);
+        }
+    }
+}
